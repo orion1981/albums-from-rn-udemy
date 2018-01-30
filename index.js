bbdf124e71a863d0c752f5099bd688a0b76fc37f
orion1981 <cstopek@hotmail.com>
2018-01-30 15:58:27 -0500

    adding code to repository

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AppRegistry, View } from 'react-native'
+import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
+
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header  headerText={"Albums"}/>
+    <AlbumList />
+  </View>
+  )
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+
+AppRegistry.registerComponent('albums', () => App);
